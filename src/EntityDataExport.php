4988ec5ccf57258bc2ec87e5ac0188133deecd3f
Goldbach07 <gustavoalves.a7@gmail.com>
2021-10-28 15:32:41 -0300

    Feat: Configure files by script

--- src/EntityDataExport.php
+++ src/EntityDataExport.php
@@ -4,29 +4,12 @@ namespace App\DataExport;
 
 class EntityDataExport{
 
-    private function getFields(){
+    public function getFields(){
+        
         return [
-            'Data de Geração' => function ($entity) {
-                return $entity->getCreatedAt()->format('d/m/Y');
-            },
-            'Número do Voucher' => function ($entity) {
-                return $entity->getCode();
-            },
-            'Código da Escola' => function ($entity) {
-                return $entity->getSchoolCode();
-            },
-            'Nome da Escola' => function ($entity) {
-                return $entity->getSchool();
-            },
-            'Cidade' => function ($entity) {
-                return $entity->getVoucherGroup()->getSchool()->getCity();
-            },
-            'Estado' => function ($entity) {
-                return $entity->getVoucherGroup()->getSchool()->getState();
-            },
-            'Status' => function ($entity) {
-                return $entity->getStatus();
-            },
+            'Id' => function ($entity) {
+                return $entity->getId();
+            }
         ];
     }
 }
